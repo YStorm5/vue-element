47534a190d7c48e5bdcb90f6f70424879b2993f6
sopanha.khun <sopanha.khun@FCHQ-BI-350>
2023-11-02 10:22:35 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
     <BaseHeader />
     <div class="flex main-container">
       <BaseSide />
-      <div w="full" py="4">
-        <Logos my="4" />
+      <div w="full" p="4">
         <HelloWorld msg="Hello Vue 3 + Element Plus + Vite" />
       </div>
     </div>
@@ -13,11 +12,10 @@
 
 <style>
 #app {
-  text-align: center;
   color: var(--ep-text-color-primary);
 }
 
 .main-container {
-  height: calc(100vh - var(--ep-menu-item-height) - 3px);
+  height: calc(100vh - var(--ep-menu-item-height) - 10px);
 }
 </style>
